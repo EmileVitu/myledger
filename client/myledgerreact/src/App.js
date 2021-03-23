e57ef7aaf31a8992ab7d99062920181fa8ed432e
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-03-23 15:30:00 +0000

    Created a basic navbar
    Nothing special, although an error because of context

--- client/myledgerreact/src/App.js
+++ client/myledgerreact/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import LedgerNavBar from './components/LedgerNavBar'
 
 function App() {
   return (
     <div className="App">
+      <LedgerNavBar />
       <h1>Building the front end</h1>
     </div>
   );
 }
 
-export default App;
+export default App
