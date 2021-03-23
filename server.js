95ccf92a0c4f81ef250ae077f5b83b44fba4f891
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-03-23 10:25:28 +0000

    Setting up database
    Created our mongoURI connect via mongoose, with console logs to inform about success or errors

--- server.js
+++ server.js
@@ -5,7 +5,14 @@
 // Importing packages and dependencies
 const express = require('express')
 const mongoose = require('mongoose')
+const db = require('./config').mongoURI
 
 // Initialising express with a variable we will call app
 const app = express()
+
+// Connecting to the database
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB for myledger connected !'))
+    .catch(err => console.log(err))
 
