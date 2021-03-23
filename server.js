591cf5d68d546bf58e271e01ff2157dcadbe6d5b
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-03-23 10:33:28 +0000

    Setting up the server running
    We set up the server to run either on port 5000 or on our future heroku server

--- server.js
+++ server.js
@@ -16,3 +16,7 @@ mongoose
     .then(() => console.log('MongoDB for myledger connected !'))
     .catch(err => console.log(err))
 
+// Setting up the server running
+const port = process.env.PORT || 5000
+    // This is to listen to our app on the port variable
+app.listen(port, () => console.log(`Server started on port ${port}`))
