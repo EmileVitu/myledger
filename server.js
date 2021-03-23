65754c9111017dc218bf83fdc2997fb6099bea7d
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-03-23 10:44:56 +0000

    Running server
    - minor error correction on dp import
    - tidied up the server.js file
    - added two options to mongoose db connect to remove deprecated stuff due to mongoDB
    - everything is fine and running good for now

--- server.js
+++ server.js
@@ -5,18 +5,24 @@
 // Importing packages and dependencies
 const express = require('express')
 const mongoose = require('mongoose')
-const db = require('./config').mongoURI
+const db = require('./config/keys').mongoURI
+
 
 // Initialising express with a variable we will call app
 const app = express()
 
+
 // Connecting to the database
 mongoose
-    .connect(db)
+    .connect(db, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    })
     .then(() => console.log('MongoDB for myledger connected !'))
     .catch(err => console.log(err))
 
+
 // Setting up the server running
 const port = process.env.PORT || 5000
-    // This is to listen to our app on the port variable
+// This is to listen to our app on the port variable
 app.listen(port, () => console.log(`Server started on port ${port}`))
