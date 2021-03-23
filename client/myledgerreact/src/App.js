2b98f47d308497435423581017f90b15abade309
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-03-23 16:30:38 +0000

    Building the list component

--- client/myledgerreact/src/App.js
+++ client/myledgerreact/src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import LedgerNavBar from './components/LedgerNavBar'
+import ExpensesList from './components/ExpensesList'
 
 function App() {
   return (
     <div className="App">
       <LedgerNavBar />
-      <h1>Building the front end</h1>
+      <h1 className="text-align-center">Welcome to my Ledger MERN app !</h1>
+      <ExpensesList />
     </div>
   );
 }
