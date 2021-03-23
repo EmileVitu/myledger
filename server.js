ec417fa1accdb579f8ceae9a1cb51dd9f05c2f84
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-03-23 10:16:36 +0000

    Importing and initialising express and mongoose

---
+++ server.js
@@ -0,0 +1,11 @@
+// MyLedger
+// Back-End main file
+
+
+// Importing packages and dependencies
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Initialising express with a variable we will call app
+const app = express()
+
