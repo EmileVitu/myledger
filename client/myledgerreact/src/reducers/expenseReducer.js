eaa1f45f245c5a1647b25e508e78ea161d4c30ca
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-03-24 15:08:39 +0000

    expenseReducer changes
    - Deleted our fake state in our expense reducer to set it to an empty array for accepting the MongoDB data
    - removed our uuid import
    - added the import of the EXPENSES_LOADING action
    - added that action export in our types.js file
    - now we can add this action to our swith function to our expenseReducer

--- client/myledgerreact/src/reducers/expenseReducer.js
+++ client/myledgerreact/src/reducers/expenseReducer.js
@@ -1,36 +1,36 @@
-import { v1 as uuid } from 'uuid'
-import { GET_EXPENSES, ADD_EXPENSE, DELETE_EXPENSE } from '../actions/types'
+// import { v1 as uuid } from 'uuid'
+import { GET_EXPENSES, ADD_EXPENSE, DELETE_EXPENSE, EXPENSES_LOADING } from '../actions/types'
 
 
 const initialState = {
     expenses: [
-        { 
-            id: uuid(), 
-            title: 'Groceries',
-            user: 'Emile',
-            amount: 15.57,
-            dateExpense: 15486, 
-            category: 'important', 
-            comment: 'Groceries for first week of january' 
-        },
-        { 
-            id: uuid(), 
-            title: 'Electricity',
-            user: 'Emile',
-            amount: 86.25,
-            dateExpense: 15555, 
-            category: 'important', 
-            comment: 'Electricity for january'
-         },
-        { 
-            id: uuid(), 
-            title: 'Rent',
-            user: 'Emile',
-            amount: 850,
-            dateExpense: 15555, 
-            category: 'important', 
-            comment: 'Rent for January' 
-        }
+        // { 
+        //     id: uuid(), 
+        //     title: 'Groceries',
+        //     user: 'Emile',
+        //     amount: 15.57,
+        //     dateExpense: 15486, 
+        //     category: 'important', 
+        //     comment: 'Groceries for first week of january' 
+        // },
+        // { 
+        //     id: uuid(), 
+        //     title: 'Electricity',
+        //     user: 'Emile',
+        //     amount: 86.25,
+        //     dateExpense: 15555, 
+        //     category: 'important', 
+        //     comment: 'Electricity for january'
+        //  },
+        // { 
+        //     id: uuid(), 
+        //     title: 'Rent',
+        //     user: 'Emile',
+        //     amount: 850,
+        //     dateExpense: 15555, 
+        //     category: 'important', 
+        //     comment: 'Rent for January' 
+        // }
     ]
 }
 
@@ -49,6 +49,11 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 expenses: state.expenses.filter(expense => expense.id !== action.payload)
+            }
+        case EXPENSES_LOADING:
+            return {
+                ...state,
+                loading: true
             }
         default:
             return state
