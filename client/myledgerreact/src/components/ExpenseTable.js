a04898ae1191f3804a492932c2e1fdc5f406966e
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-04-08 12:24:37 +0100

    azd

---
+++ client/myledgerreact/src/components/ExpenseTable.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useTable } from 'react-table'
+import { COLUMNS } from './table/columns'
+
+function ExpenseTable() {
+    return (
+        <div>
+            <h2>Table</h2>
+        </div>
+    )
+}
+
+export default ExpenseTable
---
+++ client/myledgerreact/src/components/Expenses.js
@@ -0,0 +1,67 @@
+// Dependencies
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+// Functions
+import { getExpenses, deleteExpense } from '../actions/expenseActions'
+// Styling
+import { Container, Button, List, ListItem } from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
+
+import UpdateModal from './UpdateModal'
+import ExpenseTable from './ExpenseTable'
+
+class ExpensesList extends Component {
+
+    static propTypes = {
+        getExpenses: PropTypes.func.isRequired, 
+        expense: PropTypes.object.isRequired,
+        isAuthenticated: PropTypes.bool
+    }
+
+    componentDidMount() {
+        this.props.getExpenses()
+    }
+
+    onDeleteClick = (_id) => {
+        this.props.deleteExpense(_id)
+    }
+
+    render() {
+        const { expenses } = this.props.expense
+
+        return(
+            <Container>                
+                <List>
+                    {expenses.map(({ _id, title, user, amount, category, dateExpense, comment, dateCreated }) => (
+                        <ListItem key={_id}>
+                            { this.props.isAuthenticated ? 
+                                <>
+                                    <Button 
+                                        variant='contained' 
+                                        color='secondary'
+                                        size='small'
+                                        onClick={this.onDeleteClick.bind(this, _id)}
+                                        ><DeleteIcon />Delete</Button>
+                                    <UpdateModal parentId={_id} />
+                                </> 
+                                : null }
+                            {_id} - {dateCreated} - {title} - {user} - {amount} - {dateExpense} - {category} - {comment}
+                        </ListItem>
+                    ))}
+                </List>
+                <ExpenseTable />
+            </Container>
+        )
+    }
+
+}
+
+
+const mapStateToProps = (state) => ({
+    expense: state.expense,
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+
+export default connect(mapStateToProps, { getExpenses, deleteExpense })(ExpensesList)
