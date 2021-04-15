a844569980384064bb276f1d0f90c44be70d078c
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-04-15 09:30:51 +0100

    dza

--- client/myledgerreact/src/actions/errorActions.js
+++ client/myledgerreact/src/actions/errorActions.js
@@ -1,6 +1,6 @@
 import { GET_ERRORS, CLEAR_ERRORS } from './types'
 
-// RETURN ERRORS
+// Error actions
 export const returnErrors = (msg, status, id = null) => {
     return {
         type: GET_ERRORS,
@@ -8,7 +8,6 @@ export const returnErrors = (msg, status, id = null) => {
     }
 }
 
-// CLEAR ERRORS
 export const clearErrors = () => {
     return {
         type: CLEAR_ERRORS
