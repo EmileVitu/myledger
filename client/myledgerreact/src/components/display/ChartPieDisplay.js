39f2f24472058da4b0a0448f741f66a27e116e44
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-04-16 17:41:44 +0100

    Created dummy piechart

--- client/myledgerreact/src/components/display/ChartPieDisplay.js
+++ client/myledgerreact/src/components/display/ChartPieDisplay.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 // Functions
 import { getExpenses } from '../../actions/expenseActions'
+// Styling
+import { ResponsivePie } from '@nivo/pie'
 
 
 
@@ -19,9 +21,154 @@ export class ChartPieDisplay extends Component {
     }
 
     render() {
+
+        // const { expenses } = this.props.expense
+
+        const data = [
+            {
+              "id": "css",
+              "label": "css",
+              "value": 270,
+              "color": "hsl(29, 70%, 50%)"
+            },
+            {
+              "id": "python",
+              "label": "python",
+              "value": 266,
+              "color": "hsl(97, 70%, 50%)"
+            },
+            {
+              "id": "rust",
+              "label": "rust",
+              "value": 188,
+              "color": "hsl(212, 70%, 50%)"
+            },
+            {
+              "id": "c",
+              "label": "c",
+              "value": 130,
+              "color": "hsl(173, 70%, 50%)"
+            },
+            {
+              "id": "make",
+              "label": "make",
+              "value": 29,
+              "color": "hsl(298, 70%, 50%)"
+            }
+          ]
+
         return (
-            <div>
-                <h1>Pie Display!</h1>
+            <div style={{height:500}}>
+                <ResponsivePie
+                    data={data}
+                    margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                    innerRadius={0.5}
+                    padAngle={0.7}
+                    cornerRadius={3}
+                    colors={{ scheme: 'nivo' }}
+                    borderWidth={1}
+                    borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+                    radialLabelsSkipAngle={10}
+                    radialLabelsTextColor="#333333"
+                    radialLabelsLinkColor={{ from: 'color' }}
+                    sliceLabelsSkipAngle={10}
+                    sliceLabelsTextColor="#333333"
+                    defs={[
+                        {
+                            id: 'dots',
+                            type: 'patternDots',
+                            background: 'inherit',
+                            color: 'rgba(255, 255, 255, 0.3)',
+                            size: 4,
+                            padding: 1,
+                            stagger: true
+                        },
+                        {
+                            id: 'lines',
+                            type: 'patternLines',
+                            background: 'inherit',
+                            color: 'rgba(255, 255, 255, 0.3)',
+                            rotation: -45,
+                            lineWidth: 6,
+                            spacing: 10
+                        }
+                    ]}
+                    fill={[
+                        {
+                            match: {
+                                id: 'ruby'
+                            },
+                            id: 'dots'
+                        },
+                        {
+                            match: {
+                                id: 'c'
+                            },
+                            id: 'dots'
+                        },
+                        {
+                            match: {
+                                id: 'go'
+                            },
+                            id: 'dots'
+                        },
+                        {
+                            match: {
+                                id: 'python'
+                            },
+                            id: 'dots'
+                        },
+                        {
+                            match: {
+                                id: 'scala'
+                            },
+                            id: 'lines'
+                        },
+                        {
+                            match: {
+                                id: 'lisp'
+                            },
+                            id: 'lines'
+                        },
+                        {
+                            match: {
+                                id: 'elixir'
+                            },
+                            id: 'lines'
+                        },
+                        {
+                            match: {
+                                id: 'javascript'
+                            },
+                            id: 'lines'
+                        }
+                    ]}
+                    legends={[
+                        {
+                            anchor: 'bottom',
+                            direction: 'row',
+                            justify: false,
+                            translateX: 0,
+                            translateY: 56,
+                            itemsSpacing: 0,
+                            itemWidth: 100,
+                            itemHeight: 18,
+                            itemTextColor: '#999',
+                            itemDirection: 'left-to-right',
+                            itemOpacity: 1,
+                            symbolSize: 18,
+                            symbolShape: 'circle',
+                            effects: [
+                                {
+                                    on: 'hover',
+                                    style: {
+                                        itemTextColor: '#000'
+                                    }
+                                }
+                            ]
+                        }
+                    ]}
+                />
             </div>
         )
     }
