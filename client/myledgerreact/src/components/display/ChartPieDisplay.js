5828c967e8c4c96ab9bbb7eaa263610921103ad4
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2021-04-19 17:14:53 +0100

    Created filter function

--- client/myledgerreact/src/components/display/ChartPieDisplay.js
+++ client/myledgerreact/src/components/display/ChartPieDisplay.js
@@ -20,9 +20,46 @@ export class ChartPieDisplay extends Component {
         this.props.getExpenses()
     }
 
+    sumOfAllExpenses = () => {
+        
+    }
+
     render() {
 
-        // const { expenses } = this.props.expense
+        const { expenses } = this.props.expense
+
+        const categoryFilterNeeds = expenses.filter(expense => expense.category === 'Needs')
+        console.log(categoryFilterNeeds)
+
+        const categoryFilterWants = expenses.filter(expense => expense.category === 'Wants')
+        console.log(categoryFilterWants)
+
+        const categoryFilterCulture = expenses.filter(expense => expense.category === 'Culture')
+        console.log(categoryFilterCulture)
+
+        const categoryFilterUnexpected = expenses.filter(expense => expense.category === 'Unexpected')
+        console.log(categoryFilterUnexpected)
+
+
+        // const result = expenses.reduce((r, { dateExpense, amount, category }) => {
+        //     const dateObject = new Date(dateExpense)
+        //     const total = expenses.amount*1
+        //     const monthyear = dateObject.toLocaleString('en-us', { month: 'long', year: 'numeric' })
+        //     if(!r[monthyear]){ r[monthyear] = { monthyear, entries: 1 , expense: {expenses}}}
+        //     else r[monthyear].entries++
+        //     return r
+        // }, [])
+
+        // console.log(Object.values(result))
+        // console.log(result.total)
+        // console.log(expenses.amount)
+
+
+        // First filter category --> gives the global selector
+        // Then filter years --> gives the years selector
+        // Then filter the months --> gives the months and years selector
+
+        // --> !!!!! each must return a different array to display in our pie
 
         const data = [
             {
@@ -58,118 +95,138 @@ export class ChartPieDisplay extends Component {
           ]
 
         return (
-            <div style={{height:500}}>
-                <ResponsivePie
-                    data={data}
-                    margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-                    innerRadius={0.5}
-                    padAngle={0.7}
-                    cornerRadius={3}
-                    colors={{ scheme: 'nivo' }}
-                    borderWidth={1}
-                    borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
-                    radialLabelsSkipAngle={10}
-                    radialLabelsTextColor="#333333"
-                    radialLabelsLinkColor={{ from: 'color' }}
-                    sliceLabelsSkipAngle={10}
-                    sliceLabelsTextColor="#333333"
-                    defs={[
-                        {
-                            id: 'dots',
-                            type: 'patternDots',
-                            background: 'inherit',
-                            color: 'rgba(255, 255, 255, 0.3)',
-                            size: 4,
-                            padding: 1,
-                            stagger: true
-                        },
-                        {
-                            id: 'lines',
-                            type: 'patternLines',
-                            background: 'inherit',
-                            color: 'rgba(255, 255, 255, 0.3)',
-                            rotation: -45,
-                            lineWidth: 6,
-                            spacing: 10
-                        }
-                    ]}
-                    fill={[
-                        {
-                            match: {
-                                id: 'ruby'
+            <>
+                <div style={{height:500}}>
+                    <ResponsivePie
+                        data={data}
+                        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                        innerRadius={0.5}
+                        padAngle={0.7}
+                        cornerRadius={3}
+                        colors={{ scheme: 'nivo' }}
+                        borderWidth={1}
+                        borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+                        radialLabelsSkipAngle={10}
+                        radialLabelsTextColor="#333333"
+                        radialLabelsLinkColor={{ from: 'color' }}
+                        sliceLabelsSkipAngle={10}
+                        sliceLabelsTextColor="#333333"
+                        defs={[
+                            {
+                                id: 'dots',
+                                type: 'patternDots',
+                                background: 'inherit',
+                                color: 'rgba(255, 255, 255, 0.3)',
+                                size: 4,
+                                padding: 1,
+                                stagger: true
                             },
-                            id: 'dots'
-                        },
-                        {
-                            match: {
-                                id: 'c'
+                            {
+                                id: 'lines',
+                                type: 'patternLines',
+                                background: 'inherit',
+                                color: 'rgba(255, 255, 255, 0.3)',
+                                rotation: -45,
+                                lineWidth: 6,
+                                spacing: 10
+                            }
+                        ]}
+                        fill={[
+                            {
+                                match: {
+                                    id: 'ruby'
+                                },
+                                id: 'dots'
                             },
-                            id: 'dots'
-                        },
-                        {
-                            match: {
-                                id: 'go'
+                            {
+                                match: {
+                                    id: 'c'
+                                },
+                                id: 'dots'
                             },
-                            id: 'dots'
-                        },
-                        {
-                            match: {
-                                id: 'python'
+                            {
+                                match: {
+                                    id: 'go'
+                                },
+                                id: 'dots'
                             },
-                            id: 'dots'
-                        },
-                        {
-                            match: {
-                                id: 'scala'
+                            {
+                                match: {
+                                    id: 'python'
+                                },
+                                id: 'dots'
                             },
-                            id: 'lines'
-                        },
-                        {
-                            match: {
-                                id: 'lisp'
+                            {
+                                match: {
+                                    id: 'scala'
+                                },
+                                id: 'lines'
                             },
-                            id: 'lines'
-                        },
-                        {
-                            match: {
-                                id: 'elixir'
+                            {
+                                match: {
+                                    id: 'lisp'
+                                },
+                                id: 'lines'
                             },
-                            id: 'lines'
-                        },
-                        {
-                            match: {
-                                id: 'javascript'
+                            {
+                                match: {
+                                    id: 'elixir'
+                                },
+                                id: 'lines'
                             },
-                            id: 'lines'
-                        }
-                    ]}
-                    legends={[
-                        {
-                            anchor: 'bottom',
-                            direction: 'row',
-                            justify: false,
-                            translateX: 0,
-                            translateY: 56,
-                            itemsSpacing: 0,
-                            itemWidth: 100,
-                            itemHeight: 18,
-                            itemTextColor: '#999',
-                            itemDirection: 'left-to-right',
-                            itemOpacity: 1,
-                            symbolSize: 18,
-                            symbolShape: 'circle',
-                            effects: [
-                                {
-                                    on: 'hover',
-                                    style: {
-                                        itemTextColor: '#000'
+                            {
+                                match: {
+                                    id: 'javascript'
+                                },
+                                id: 'lines'
+                            }
+                        ]}
+                        legends={[
+                            {
+                                anchor: 'bottom',
+                                direction: 'row',
+                                justify: false,
+                                translateX: 0,
+                                translateY: 56,
+                                itemsSpacing: 0,
+                                itemWidth: 100,
+                                itemHeight: 18,
+                                itemTextColor: '#999',
+                                itemDirection: 'left-to-right',
+                                itemOpacity: 1,
+                                symbolSize: 18,
+                                symbolShape: 'circle',
+                                effects: [
+                                    {
+                                        on: 'hover',
+                                        style: {
+                                            itemTextColor: '#000'
+                                        }
                                     }
-                                }
-                            ]
-                        }
-                    ]}
-                />
-            </div>
+                                ]
+                            }
+                        ]}
+                    />
+                </div>
+                <div>
+                    <p>Needs</p>
+                    {categoryFilterNeeds.map(({ _id, category, amount, dateExpense }) => (
+                        <li key={_id}>{category} - {amount} - {dateExpense}</li>
+                    ))}
+                    <p>Wants</p>
+                    {categoryFilterWants.map(({ _id, category, amount, dateExpense }) => (
+                        <li key={_id}>{category} - {amount} - {dateExpense}</li>
+                    ))}
+                    <p>Culture</p>
+                    {categoryFilterCulture.map(({ _id, category, amount, dateExpense }) => (
+                        <li key={_id}>{category} - {amount} - {dateExpense}</li>
+                    ))}
+                    <p>Unexpected</p>
+                    {categoryFilterUnexpected.map(({ _id, category, amount, dateExpense }) => (
+                        <li key={_id}>{category} - {amount} - {dateExpense}</li>
+                    ))}
+                </div>
+            </>
         )
     }
 }
